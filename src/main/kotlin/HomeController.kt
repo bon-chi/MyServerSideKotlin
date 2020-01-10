e7626b5c7e1cda795dad129157dcd8bf75f7fd22
Koji Ota <afterjnih@gmail.com>
2020-01-10 16:58:50 +0900

    use web.xml

--- src/main/kotlin/HomeController.kt
+++ src/main/kotlin/HomeController.kt
@@ -3,9 +3,9 @@ import javax.servlet.http.HttpServlet
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
-@WebServlet(name = "Hello", value = ["/hello"])
+//@WebServlet(name = "Hello", value = ["/hello"])
 class HomeController : HttpServlet() {
     override fun doGet(req: HttpServletRequest, res: HttpServletResponse) {
-        res.writer.write("Hello, World!")
+        res.writer.write("Hello, World!!!")
     }
 }
